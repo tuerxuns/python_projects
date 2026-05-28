2afc5567864f174d5b0771151e01696266820336
tuerxuns <tuerxuns@gmail.com>
2026-05-27 20:12:28 -0700

    Add FizzBuzz implementation
    The function prints numbers from 1 to a given limit, replacing multiples
    of 3 with "Fizz", multiples of 5 with "Buzz", and multiples of both with
    "FizzBuzz". Includes a sample call with upTo=35.

---
+++ 05_fizz_buzz.py
@@ -0,0 +1,13 @@
+def fizzBuzz(upTo):
+    for n in range(1, upTo + 1):
+        if (n % 3) == 0 and (n % 5) == 0:
+            print("FizzBuzz")
+        elif (n % 3) == 0:
+            print("Fizz")
+        elif (n % 5) == 0:
+            print("Buzz")
+        else:
+            print(n)
+
+
+fizzBuzz(35)
