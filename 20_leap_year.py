a7b751010b399cb1c90c5eb44e0f9c09c0a817e7
tuerxuns <tuerxuns@gmail.com>
2026-06-01 19:39:52 -0700

    Add leap year function with test cases

---
+++ 20_leap_year.py
@@ -0,0 +1,24 @@
+def isLeapYear(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            return False
+        return True
+    else:
+        return False
+
+
+assert isLeapYear(1999) == False
+
+assert isLeapYear(2000) == True
+
+assert isLeapYear(2001) == False
+
+assert isLeapYear(2004) == True
+
+assert isLeapYear(2100) == False
+
+assert isLeapYear(2400) == True
+
+print("All tests passed!")
